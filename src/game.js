a16f98c2de6a392d1af2dfc49e197e4d2e952767
Nicklas Löf <nicklaslof76@gmail.com>
2026-08-27 17:16:44 +0200

    First commit in a few days..
    Got sick with eye problems :(
    
    But added UI and a font plus small level changes

--- src/game.js
+++ src/game.js
@@ -10,6 +10,7 @@ import MathUtil from "./mathutil.js";
 import Structures from "./structure/structures.js";
 
 import {zzfx} from './lib/z.js'
+import UI from "./ui/ui.js";
 
 
 
@@ -32,6 +33,8 @@ export default class Game{
 
         this.input = new Input();
         this.canvas.addEventListener('click', (e) => { this.canvas.requestPointerLock(); this.mouseLocked = true;});
+
+        this.ui = new UI(852*2,480*2);
 
         Game.shaderProgram = new ShaderProgram(Game.gl,`precision highp float; attribute vec4 p; attribute vec4 c; attribute vec4 l; attribute vec2 u; uniform float rX; uniform float rY; uniform float crX; uniform float crY; uniform vec3 cp; uniform vec3 mp; uniform vec3 ms; varying vec4 vc; varying vec2 uv; varying float d; varying vec4 li; mat4 rmX(float rX){ return mat4( 1,0,0,0, 0,cos(rX),-sin(rX),0, 0,sin(rX),cos(rX),0, 0,0,0,1); } mat4 rmY(float rY){ return mat4( cos(rY),0,sin(rY),0, 0,1,0,0, -sin(rY),0,cos(rY),0, 0,0,0,1 ); } mat4 cpm(float fov, float aspect, float near, float far) { float f = 1.0 / tan(fov / 2.0); return mat4( f / aspect, 0.0, 0.0, 0.0, 0.0, f, 0.0, 0.0, 0.0, 0.0, (far + near) / (near - far), -1.0, 0.0, 0.0, (2.0 * far * near) / (near - far), 0.0 ); } void main(){ vec4 rp = rmX(rX) * rmY(rY) * vec4(p.x*ms.x,p.y*ms.y,p.z*ms.z,p.w) + vec4(mp-cp,1.0); mat4 proj = cpm(1.2,2.0,0.1,40.0) * rmX(crX) * rmY(crY); gl_Position = proj * rp; vc=c; li=l; uv=u; }`,` precision highp float; varying vec4 vc; varying vec2 uv; varying float d; varying vec4 li; uniform sampler2D s; void main(){ vec4 col=texture2D(s,uv)*vc;vec4 c=vec4(col.rgb,col.a)*li; if (col.rgb == vec3(0.0,0.0,0.0)) discard; gl_FragColor=c; }`);
 
@@ -176,6 +179,7 @@ export default class Game{
 
     render(){
         this.level.render();
+        this.ui.render(this);
     }
 
     playShoot(){
---
+++ src/ui/ui.js
@@ -0,0 +1,45 @@
+import Game from "../game.js";
+
+export default class UI{
+    constructor(width,height){
+        this.canvas = document.getElementById("u");
+        this.canvas.width = width;
+        this.canvas.height = height;
+        this.context = this.canvas.getContext("2d");
+        this.context.imageSmoothingEnabled = false;
+
+
+
+    }
+
+    tick(deltaTime){
+
+    }
+
+    render(game){
+        this.context.clearRect(0,0,this.canvas.width,this.canvas.height);
+
+        //this.drawTextAt(game.level.player.position.x.toFixed(2)+" "+game.level.player.position.z.toFixed(2), 100,64,18);
+        this.drawTextAt("abcdefghijklmnopqrstuvwxyz",100,64,18);
+        this.context.drawImage(Game.glTexture.image,0,59,5,5,(this.canvas.width/2)-8,(this.canvas.height/2)-2,16,16);
+
+    }
+
+
+    drawTextAt(text,x,y,size){
+        for (let i = 0; i < text.length; i++) {
+            var c = text.toUpperCase().charCodeAt(i);
+            var idx = c - (c < 64 ? 48 : 55);
+            var row = idx < 12 ? 0 : idx < 24 ? 1 : 2;
+            idx %= 12;
+            this.context.drawImage(Game.glTexture.image, idx * 5, 44 + row * 5, 5, 5, x, y, size, size);
+            x += size + 5;
+        }
+        /*this.context.font=font==null?"14px monospace":font;
+        this.context.fillStyle = "white";
+        this.context.textAlign = "center";
+        this.context.fillText(text,x,y);*/
+    }
+
+
+}
