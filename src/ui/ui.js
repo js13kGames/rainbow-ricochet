98c9ca4d5beaca5ba967d5ffb4e2981832c4b064
Nicklas Löf <nicklaslof76@gmail.com>
2026-08-27 18:52:49 +0200

    Removed the custom font and code. I can use the 300 bytes on content instead.

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -20,25 +20,17 @@ export default class UI{
         this.context.clearRect(0,0,this.canvas.width,this.canvas.height);
         this.context.drawImage(Game.glTexture.image,0,59,5,5,(this.canvas.width/2)-8,(this.canvas.height/2)-2,16,16);
 
-        this.drawTextAt("health",10,40,14);
-        this.drawTextAt(""+game.player.health, 140,38,18);
+        this.drawTextAt("HEALTH:",56,40,24);
+        this.drawTextAt(game.player.health, 126,42,26,"red");
 
     }
 
 
-    drawTextAt(text,x,y,size){
-        for (let i = 0; i < text.length; i++) {
-            var c = text.toUpperCase().charCodeAt(i);
-            var idx = c - (c < 64 ? 48 : 55);
-            var row = idx < 12 ? 0 : idx < 24 ? 1 : 2;
-            idx %= 12;
-            this.context.drawImage(Game.glTexture.image, idx * 5, 44 + row * 5, 5, 5, x, y, size, size);
-            x += size + 5;
-        }
-        /*this.context.font=font==null?"14px monospace":font;
-        this.context.fillStyle = "white";
+    drawTextAt(text,x,y,size,color="white"){
+        this.context.font=size+"px monospace";
+        this.context.fillStyle = color;
         this.context.textAlign = "center";
-        this.context.fillText(text,x,y);*/
+        this.context.fillText(text,x,y);
     }
 
 
