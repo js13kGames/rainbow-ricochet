1f456a0dcad63d488037cd6f9dedcfee6788bab6
Nicklas Löf <nicklaslof76@gmail.com>
2026-09-02 19:12:08 +0200

    Poison texture fixed.
    After fixing graphicproblems on windows.

--- src/structure/poision.js
+++ src/structure/poision.js
@@ -4,7 +4,7 @@ import Structure from "./structure.js";
 export default class Poision extends Structure{
     constructor(glTexture) {
         //super(new Texture(glTexture,63,0,1,1));
-        super(new Texture(glTexture,0,0,16,16));
+        super(new Texture(glTexture,16,48,16,16));
         this.tint = [0.0,0.9,0.0,1.0];
     }
 
