25cb5c632eb9bb23cda302d7e18bb0136e850bd8
Nicklas Löf <nicklaslof76@gmail.com>
2026-09-07 18:40:29 +0200

    level exit has rainbow colors

--- src/entity/levelexit.js
+++ src/entity/levelexit.js
@@ -16,8 +16,8 @@ export default class LevelExit extends Entity{
     tick(game,deltaTime){
         super.tick(game,deltaTime);
         if (Math.random()< 0.1){
-            //var c = Game.rainbowColors[Math.floor(Math.random()*6)];
-            game.level.addParticle(new Particle(game.level,(this.position.x-0.5)+Math.random(),this.position.y,(this.position.z-0.5)+Math.random(),6,{x:0,y:1,z:0},Math.max(0.01,Math.random()/80),[0.0,1.0,0.0,0.9],0.05));
+            var c = Game.rainbowColors[Math.floor(Math.random()*6)];
+            game.level.addParticle(new Particle(game.level,(this.position.x-0.5)+Math.random(),this.position.y,(this.position.z-0.5)+Math.random(),9,{x:0,y:1,z:0},Math.max(0.01,Math.random()/20),[c[0],c[1],c[2],1.0],0.09));
         }
     }
 }
